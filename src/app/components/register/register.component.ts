929f40a837394798a1e77679a6a2c19933bf1c48
ArminCicak <armincicak@gmailcom>
2021-02-08 22:16:17 +0100

    register komponente uspostavljena komunikacija sa API-jem

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GlobalniService } from 'src/app/globalni.service';
 
 @Component({
   selector: 'app-register',
@@ -8,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
 
 export class RegisterComponent implements OnInit {
 
+  register: FormGroup = new FormGroup({​​ }​​);
+ porukaZaKorisnika: string = "";
 
-  constructor() { }
+  constructor(private api: GlobalniService, private router: Router) { }
 
   ngOnInit() {
+
+    this.register = new FormGroup({​​
+
+      CardNumber: new FormControl('', Validators.required),
+
+      IndexNumber: new FormControl ('', Validators.required),
+
+      Email: new FormControl('', Validators.required),
+
+      });
+
+
+
+    }
+
+
+  onSubmit (novaRegistracija: FormGroup) {​​
+
+  this.api.postRegister(novaRegistracija.value).subscribe((rezultat) => {
+
+    console.log(rezultat);
+
+    if (rezultat.Status=="Success"){
+
+      this.porukaZaKorisnika = "Registracija je uspješna. Na Vaš email je poslan gereički password.";
+
+
+
+    setTimeout(() => {​​
+
+      this.router.navigate(['/signin'])
+
+    }​​, 1000);
+
   }
+
+  else  {
+
+   this.porukaZaKorisnika = "Pogrešan broj kartice ili indexa.";
+  }
+
+  });
+
+}
 
 }
