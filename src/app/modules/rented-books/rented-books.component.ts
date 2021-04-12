ad99b2a22e1d2b7c62c955d8999d7a939039ec3e
ArminCicak <armincicak@gmailcom>
2021-04-12 17:49:10 +0200

    dodana komponenta iznajmljene knjige

---
+++ src/app/modules/rented-books/rented-books.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+
+];
+
+@Component({
+  selector: 'app-rented-books',
+  templateUrl: './rented-books.component.html',
+  styleUrls: ['./rented-books.component.css']
+})
+export class RentedBooksComponent implements OnInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, {static: true}) paginator; any: any[] | undefined; MatPaginator;
+
+
+  constructor() {
+
+      }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+
+}
